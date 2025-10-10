1756c3bc4ca40ad8af809420164b5934d046dc50
Phil-the-amature-dev <98315948+Phil-the-amature-dev@users.noreply.github.com>
2025-10-10 20:22:23 +0200

    movement and projectile progress
    firs iteration of movement is done, started working on projectiles (WIP)

--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -1,24 +1,63 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int speed;
+    public float mouseSens;
+    public Camera playerCamera;
+    public Transform groundCheck;
+    public float groundCheckExtends;
+    public LayerMask groundLayer;
+    public Transform bombSpawn;
+    public Transform bomb;
+    float camRotation;
+    
+    bool canJump;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
+        //movement
         transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0); 
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+        //Horizontal Rotation
+        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+        //vertical cam rotation
+        camRotation -= Input.GetAxis("Mouse Y");
+        camRotation = Mathf.Clamp(camRotation, -8f, 45f);
+        playerCamera.transform.localRotation = Quaternion.Euler(camRotation, 0, 0);
+        
 
-        if (Input.GetKeyDown("space"))
+
+
+
+        //jumping
+        if (Input.GetKeyDown("space") && IsGrounded())
         {
+            canJump = false;
             transform.GetComponent<Rigidbody>().AddForce(transform.up, ForceMode.Impulse);
+            Debug.Log(canJump);
         }
 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bomb, bombSpawn.position, Quaternion.Euler(0,0,0));
+            bomb.GetComponent<Rigidbody>().AddForce(transform.forward, ForceMode.Force);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        //Grounded check
+        return Physics.Raycast(transform.GetComponent<Collider>().bounds.center, -transform.up, transform.GetComponent<Collider>().bounds.extents.y + groundCheckExtends, groundLayer);
     }
 }
